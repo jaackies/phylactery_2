1717de2e1f35ecae366423f8d7cd00453388ea5a
Donald Sutherland <donald@sutherland.id.au>
2023-12-17 17:48:40 +0800

    Add item model

--- library/models.py
+++ library/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    """
+    Stores all the data related to a single library item.
+    """
+
+    # The name and slug of the Item
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100)
+
+    # These fields are for the description, condition, and other notes.
+    description = models.TextField(blank=True)
+    condition = models.TextField(blank=True)
+    notes = models.TextField(blank=True)
+
+    min_players = models.PositiveIntegerField(blank=True, null=True)
+    max_players = models.PositiveIntegerField(blank=True, null=True)
+
+    min_play_time = models.PositiveIntegerField(blank=True, null=True, help_text="in minutes")
+    max_play_time = models.PositiveIntegerField(blank=True, null=True, help_text="in minutes")
+    average_play_time = models.PositiveIntegerField(blank=True, null=True, help_text="in minutes")
+
+    # These govern the Item's borrowing behaviour
+    is_borrowable = models.BooleanField(default=True)
+    is_high_demand = models.BooleanField(default=False)
+
+    def get_image_filename(self, filename: str) -> str:
+        filename, _, extension = filename.rpartition('.')
+        return f"library/item_images/{self.slug}.{extension}"
+
+    image = models.ImageField(upload_to=get_image_filename, null=True)
+
+    class Meta:
+        ordering = ['name']
+
