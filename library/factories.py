15f036186fad7178c22f8bade8390f6600765386
Donald Sutherland <donald@sutherland.id.au>
2023-12-19 11:32:27 +0800

    Add ItemFactory

---
+++ library/factories.py
@@ -0,0 +1,24 @@
+from django.utils.text import slugify
+import factory
+from .models import Item, LibraryTag
+
+
+class ItemFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Item
+
+    name = factory.Sequence(lambda n: f"Item {n}")
+    slug = factory.Sequence(lambda n: slugify(f"Item {n}"))
+    description = None
+    condition = None
+    notes = None
+
+    min_players = None
+    max_players = None
+    min_play_time = None
+    max_play_time = None
+
+    is_borrowable = True
+    is_high_demand = False
+
+    image = None
