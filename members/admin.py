1be862d782512f40245e023f641ea8514320abc6
donsuth <donald@sutherland.id.au>
2023-12-25 17:01:32 +0800

    Update the admin for members

--- members/admin.py
+++ members/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 
-from .models import Member, Rank
+from .models import Member, Membership, Rank
 
-admin.site.register(Member)
-admin.site.register(Rank)
+
+class RankInline(admin.TabularInline):
+	model = Rank
+	extra = 1
+
+
+class MembershipInline(admin.TabularInline):
+	model = Membership
+	extra = 0
+	fk_name = "member"
+	fields = ("date_purchased", "phone_number", "guild_member", "amount_paid", "expired", "authorised_by")
+
+
+class MemberAdmin(admin.ModelAdmin):
+	inlines = [RankInline, MembershipInline]
+
+
+admin.site.register(Member, MemberAdmin)
 
 # Register your models here.
