a7a9e97d9b8e22f7e9445c02298c6c0d94e0648e
Donald Sutherland <donald@sutherland.id.au>
2024-01-01 11:34:38 +0800

    Add factories for more testing

--- library/factories.py
+++ library/factories.py
@@ -1,6 +1,6 @@
 from django.utils.text import slugify
 import factory
-from .models import Item, LibraryTag
+from .models import Item, LibraryTag, BorrowerDetails, BorrowRecord, Reservation
 
 
 class ItemFactory(factory.django.DjangoModelFactory):
@@ -29,3 +29,46 @@ class LibraryTagFactory(factory.django.DjangoModelFactory):
 		model = LibraryTag
 	
 	name = factory.Sequence(lambda n: f"tag{n}")
+
+
+class BorrowerDetailsFactory(factory.django.DjangoModelFactory):
+	class Meta:
+		model = BorrowerDetails
+	
+	is_external = True
+	internal_member = None
+	borrower_name = factory.Faker("name")
+	
+	borrower_address = factory.Faker("address")
+	borrower_phone = factory.Faker("phone_number")
+
+
+class BorrowRecordFactory(factory.django.DjangoModelFactory):
+	class Meta:
+		model = BorrowRecord
+	
+	item = factory.SubFactory(ItemFactory)
+	borrower = factory.SubFactory(BorrowerDetailsFactory)
+	
+	borrow_authorised_by = factory.Faker("name")
+	
+
+class ReservationFactory(factory.django.DjangoModelFactory):
+	class Meta:
+		model = Reservation
+	
+	is_external = False
+	internal_member = None
+	requestor_name = factory.Faker("name")
+	requestor_email = factory.Faker("email")
+	requestor_phone = factory.Faker("phone_number")
+	
+	@factory.post_generation
+	def reserved_items(self, create, extracted, **kwargs):
+		if not create or not extracted:
+			# Simple Build, or nothing to add, do nothing
+			return
+		
+		# Add the items to the reservation using bulk addition
+		self.reserved_items.add(*extracted)
+	
