58ef10dd53ad8669b74dcc046f9de6370b8ca47c
Donald Sutherland <donald@sutherland.id.au>
2024-01-03 14:26:49 +0800

    Modify member admin

--- members/admin.py
+++ members/admin.py
@@ -5,14 +5,14 @@ from .models import Member, Membership, Rank
 
 class RankInline(admin.TabularInline):
 	model = Rank
-	extra = 1
+	extra = 0
 
 
 class MembershipInline(admin.TabularInline):
 	model = Membership
 	extra = 0
 	fk_name = "member"
-	fields = ("date_purchased", "phone_number", "guild_member", "amount_paid", "expired", "authorised_by")
+	fields = ("date_purchased", "guild_member", "amount_paid", "expired", "authorised_by")
 
 
 class MemberAdmin(admin.ModelAdmin):
