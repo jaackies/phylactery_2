f7dd54b1f731ff674619042853469c2e33fe8f27
Donald Sutherland <donald@sutherland.id.au>
2024-01-09 12:25:07 +0800

    Prototype view decorators

---
+++ members/decorators.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.decorators import user_passes_test
+from .models import RankChoices
+
+
+def rank_required():
+	pass
+
+
+def gatekeeper_required():
+	pass
+
+
+def committee_required():
+	pass
+
+
