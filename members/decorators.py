06c27a00584d34ac7a1abcafb8950d70d38514b4
Donald Sutherland <donald@sutherland.id.au>
2024-01-09 16:46:45 +0800

    Finish the decorators

--- members/decorators.py
+++ members/decorators.py
@@ -1,16 +1,72 @@
 from django.contrib.auth.decorators import user_passes_test
-from .models import RankChoices
+from django.core.exceptions import PermissionDenied
 
 
-def rank_required():
-	pass
+def gatekeeper_required(function=None):
+	"""
+	Decorator for views - requires a Gatekeeper to be logged in.
+	If they are logged in, but not a Gatekeeper, then raise 403.
+	Otherwise, redirect them to the login page.
+	"""
+	
+	def gatekeeper_test(u):
+		if u.is_authenticated:
+			if u.member is not None and u.member.is_gatekeeper():
+				return True
+			else:
+				raise PermissionDenied
+		else:
+			return False
+		
+	actual_decorator = user_passes_test(gatekeeper_test)
+	
+	if function:
+		return actual_decorator(function)
+	return actual_decorator
 
 
-def gatekeeper_required():
-	pass
+def committee_required(function=None):
+	"""
+	Decorator for views - requires a Committee Member to be logged in.
+	If they are logged in, but not Committee, then raise 403.
+	Otherwise, redirect them to the login page.
+	"""
+	
+	def committee_test(u):
+		if u.is_authenticated:
+			if u.member is not None and u.member.is_committee():
+				return True
+			else:
+				raise PermissionDenied
+		else:
+			return False
+	
+	actual_decorator = user_passes_test(committee_test)
+	
+	if function:
+		return actual_decorator(function)
+	return actual_decorator
 
 
-def committee_required():
-	pass
-
-
+def exec_required(function=None):
+	"""
+	Decorator for views - requires an Executive Committee Member to be logged in.
+	(Being either President, VP, Treasurer, Secretary, or Librarian)
+	If they are logged in, but not Exec, then raise 403.
+	Otherwise, redirect them to the login page.
+	"""
+	
+	def exec_test(u):
+		if u.is_authenticated:
+			if u.member is not None and u.member.is_exec():
+				return True
+			else:
+				raise PermissionDenied
+		else:
+			return False
+	
+	actual_decorator = user_passes_test(exec_test)
+	
+	if function:
+		return actual_decorator(function)
+	return actual_decorator
