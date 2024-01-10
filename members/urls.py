9951396d2e5f3966ef6b18db2b948ccdcb71ab56
Donald Sutherland <donald@sutherland.id.au>
2024-01-10 23:17:59 +0800

    Add the Member list

--- members/urls.py
+++ members/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from members.wizards import FresherMembershipWizard, StaleMembershipWizard, LegacyMembershipWizard
+from members.views import MemberListView
 
 urlpatterns = [
 	path("signup/fresher/", FresherMembershipWizard.as_view()),
 	path("signup/stale/<int:pk>/", StaleMembershipWizard.as_view()),
-	path("signup/legacy/", LegacyMembershipWizard.as_view())
+	path("signup/legacy/", LegacyMembershipWizard.as_view()),
+	path("list/", MemberListView.as_view())
 ]
--- members/views.py
+++ members/views.py
@@ -1,10 +1,25 @@
+from django.db.models import Q
 from django.views.generic import ListView
 from .models import Member
 
 
 class MemberListView(ListView):
 	model = Member
+	paginate_by = 50
+	template_name = "members/member_list.html"
+	
+	search_query = None
 	
 	def get_queryset(self):
 		qs = Member.objects.all()
+		
+		self.search_query = self.request.GET.get("search")
+		if self.search_query:
+			qs = qs.filter(Q(short_name__icontains=self.search_query) | Q(long_name__icontains=self.search_query))
+		
 		return qs
+	
+	def get_context_data(self, *args, **kwargs):
+		context = super().get_context_data(*args, **kwargs)
+		context["search_query"] = self.search_query
+		return context
