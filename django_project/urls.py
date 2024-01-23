5bb2dc7cc14a751c6c7827849c09303974bd7166
donsuth <donald@sutherland.id.au>
2024-01-23 12:38:15 +0800

    Add URL for view

--- django_project/urls.py
+++ django_project/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 	path("admin/", admin.site.urls),
 	path("accounts/", include("allauth.urls")),
 	path("members/", include("members.urls")),
+	path("library/", include("library.urls")),
 	path("", include("pages.urls")),
 ]
 
