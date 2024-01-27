88aed5a336bc5ded908eda3d66a39525b7394da5
donsuth <donald@sutherland.id.au>
2024-01-27 23:36:12 +0800

    Add the detailview url

--- library/urls.py
+++ library/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from library.wizards import InternalBorrowItemsWizard
+from library.views import ItemDetailView
 
 urlpatterns = [
+	path("item/<slug:slug>/", ItemDetailView.as_view()),
 	path("borrow/", InternalBorrowItemsWizard.as_view(), name="borrow"),
 ]
