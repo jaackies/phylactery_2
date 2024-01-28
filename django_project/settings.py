99aedb017615094d3d54dca7bdb58faed62d45a2
donsuth <donald@sutherland.id.au>
2024-01-28 23:35:19 +0800

    Explicitly define media root

--- django_project/settings.py
+++ django_project/settings.py
@@ -141,6 +141,9 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "/media/"
+
 # https://whitenoise.readthedocs.io/en/latest/django.html
 STORAGES = {
 	"default": {
