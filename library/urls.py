f35cacca7f643d8d5217127c620c7c815a5aef7d
donsuth <donald@sutherland.id.au>
2024-01-29 08:46:12 +0800

    Add the URLs to link to those autocomplete views

--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from library.autocompletes import ItemAutocomplete, LibraryTagAutocomplete
 from library.wizards import InternalBorrowItemsWizard
 from library.views import ItemDetailView
 
 urlpatterns = [
 	path("item/<slug:slug>/", ItemDetailView.as_view()),
 	path("borrow/", InternalBorrowItemsWizard.as_view(), name="borrow"),
+	path("autocomplete-item", ItemAutocomplete.as_view(), name="autocomplete-item"),
+	path("autocomplete-tag", LibraryTagAutocomplete.as_view(), name="autocomplete-tag"),
 ]
