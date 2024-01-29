483673bc1a1bc3b216e91307e4d3dc2debf04ffd
donsuth <donald@sutherland.id.au>
2024-01-29 08:43:18 +0800

    Add Autocomplete views for Items and Tags

---
+++ library/autocompletes.py
@@ -0,0 +1,27 @@
+from dal import autocomplete
+from library.models import Item, LibraryTag
+
+
+class ItemAutocomplete(autocomplete.Select2QuerySetView):
+	"""
+	Simple view for handling the Item selection box autocompletes.
+	See the django-autocomplete-light documentation for more info.
+	"""
+	def get_queryset(self):
+		qs = Item.objects.all()
+		if self.q:
+			# TODO: Add the un-accent Postgres filter.
+			qs = qs.filter(name__istartswith=self.q)
+		return qs
+
+
+class LibraryTagAutocomplete(autocomplete.Select2QuerySetView):
+	"""
+	Simple view for handling the Tag selection box autocompletes.
+	See the django-autocomplete-light documentation for more info.
+	"""
+	def get_queryset(self):
+		qs = LibraryTag.objects.all()
+		if self.q:
+			qs = qs.filter(name__istartswith=self.q)
+		return qs
