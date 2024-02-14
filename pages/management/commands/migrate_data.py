0540c328bebc26d09ccc2ca58835350de03f6486
Donald Sutherland <donald@sutherland.id.au>
2024-02-14 20:35:14 +0800

    Add tags as well

--- pages/management/commands/migrate_data.py
+++ pages/management/commands/migrate_data.py
@@ -1,5 +1,5 @@
 import json
-from library.models import Item
+from library.models import Item, LibraryTag
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
@@ -21,10 +21,36 @@ BASE_PATH = settings.BASE_DIR / "pretty_models"
 
 class Command(BaseCommand):
 	def handle(self, *args, **options):
+		self.import_initial_library()
+	
+	def import_initial_library(self):
+		"""
+		Steps:
+			1) Import all taggit tags into LibraryTags
+			2) Import the TagParents
+			3) Add the Item Type tags (afterward, so the PKs don't conflict)
+			4) Import the Items
+			5) Add the appropriate tags onto Library Items (only base tags)
+			At this point we will need to add Members, so that we can add BorrowRecords.
+		"""
+		with open(BASE_PATH / "taggit.tag.json", "r") as json_infile:
+			json_objects = json.load(json_infile)
+		for library_tag in json_objects:
+			self.import_library_tag(pk=library_tag["pk"], fields=library_tag["fields"])
+		
 		with open(BASE_PATH / "library.item.json", "r") as json_infile:
-			library_items = json.load(json_infile)
-		for library_item in library_items:
+			json_objects = json.load(json_infile)
+		for library_item in json_objects:
 			self.import_library_item(pk=library_item["pk"], fields=library_item["fields"])
+	
+	def import_library_tag(self, pk, fields):
+		tag_data = {
+			"pk": pk,
+			"name": fields["name"],
+			"slug": fields["slug"],
+		}
+		new_tag = LibraryTag.objects.create(**tag_data)
+		self.stdout.write(f"Added library.librarytag: {new_tag.name}")
 	
 	def import_library_item(self, pk, fields):
 		item_data = {
