829b914157d173d247edcebfc5ade3eadd297649
Donald Sutherland <donald@sutherland.id.au>
2024-02-14 23:29:29 +0800

    More progress

--- pages/management/commands/migrate_data.py
+++ pages/management/commands/migrate_data.py
@@ -2,6 +2,7 @@ import json
 from library.models import Item, LibraryTag
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from collections import defaultdict
 
 """
 This program will be used to import data from the old website into the new one.
@@ -31,6 +32,7 @@ class Command(BaseCommand):
 			3) Add the Item Type tags (afterward, so the PKs don't conflict)
 			4) Import the Items
 			5) Add the appropriate tags onto Library Items (only base tags)
+			6) Save all Items.
 			At this point we will need to add Members, so that we can add BorrowRecords.
 		"""
 		
@@ -61,6 +63,39 @@ class Command(BaseCommand):
 			json_objects = json.load(json_infile)
 		for library_item in json_objects:
 			self.import_library_item(pk=library_item["pk"], fields=library_item["fields"])
+		
+		# Step 5: Apply base tags to Items.
+		# Because of the previous implementation, we have to look
+		# at two files simultaneously to figure out which tags should go where.
+		with open(BASE_PATH / "library.itembasetags.json", "r") as json_infile:
+			json_objects = json.load(json_infile)
+		# Construct a dictionary, mapping the pk of the "item_base_tags" object to the Item itself.
+		# (Yes, this system was stupid. In my defense, so was I.)
+		item_lookup = {}
+		for item_base_tags in json_objects:
+			item_lookup[item_base_tags["pk"]] = item_base_tags["fields"]["item"]
+		# Now we can continue.
+		with open(BASE_PATH / "taggit.taggeditem.json", "r") as json_infile:
+			json_objects = json.load(json_infile)
+		invalid_content_types = defaultdict(list)
+		for tagged_item in json_objects:
+			tag_pk = tagged_item["fields"]["tag"]
+			content_type = tagged_item["fields"]["content_type"]
+			object_id = tagged_item["fields"]["object_id"]
+			# The content_type is what matters here. We're looking for 12, and we ignore 13.
+			# A tag shouldn't have been tagged to anything else.
+			if content_type == 12:
+				item = Item.objects.get(pk=item_lookup[object_id])
+				tag = LibraryTag.objects.get(pk=tag_pk)
+				item.base_tags.add(tag)
+				self.stdout.write(f"Tagged {item.name} with {tag.name}")
+			elif content_type == 13:
+				pass
+			else:
+				invalid_content_types[content_type].append((tag_pk, content_type, object_id))
+		if invalid_content_types:
+			self.stdout.write(f"Warning: {invalid_content_types=}")
+		
 	
 	def import_library_tag(self, pk, fields):
 		tag_data = {
