d9a87ea9e2dba9c887f872e3229da3c817d0d05a
Donald Sutherland <donald@sutherland.id.au>
2024-06-19 15:05:29 +0800

    Add more URLs for reservation borrowing

--- library/urls.py
+++ library/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from library.autocompletes import ItemAutocomplete, LibraryTagAutocomplete
-from library.wizards import InternalBorrowItemsWizard, ReservationBorrowItemsWizard
+from library.wizards import (
+	InternalBorrowItemsWizard,
+	ReservationBorrowItemsWizard,
+	InternalReservationBorrowItemsWizard,
+	ExternalReservationBorrowItemsWizard,
+)
 from library.views import (
 	ItemDetailView,
 	ItemListView,
@@ -29,6 +34,8 @@ urlpatterns = [
 	path("tags/", TagListView.as_view(), name="tag-list"),
 	path("borrow/", InternalBorrowItemsWizard.as_view(), name="borrow-wizard"),
 	path("borrow/reservation/<int:pk>/", ReservationBorrowItemsWizard.as_view(), name="borrow-reservation"),
+	path("borrow/reservation/internal/<int:pk>/", InternalReservationBorrowItemsWizard.as_view(), name="borrow-internal-reservation"),
+	path("borrow/reservation/external/<int:pk>/", ExternalReservationBorrowItemsWizard.as_view(), name="borrow-external-reservation"),
 	path("return/<int:pk>/", ReturnItemsView.as_view(), name="return"),
 	path("autocomplete-item", ItemAutocomplete.as_view(), name="autocomplete-item"),
 	path("autocomplete-tag", LibraryTagAutocomplete.as_view(), name="autocomplete-tag"),
