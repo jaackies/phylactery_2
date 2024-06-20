746be96deac92c53f436d575a40351592e5e6560
donsuth <donald@sutherland.id.au>
2024-06-20 16:03:05 +0800

    First draft of blogpost views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import BlogPost
 
-# Create your views here.
+
+class AllBlogPostsView(ListView):
+	"""
+	View that shows a list of all BlogPosts.
+	TODO: Let Committee see everything.
+	"""
+	
+	template_name = "blog/blog_list_view.html"
+	context_object_name = "blogpost_list"
+	model = BlogPost
+	paginate_by = 10
+	
