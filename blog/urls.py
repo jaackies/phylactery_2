10e6376967f1197451ca835ca8595618f4c8ca57
donsuth <donald@sutherland.id.au>
2024-06-28 13:54:46 +0800

    Add a URL

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AllBlogPostsView
+from .views import AllBlogPostsView, BlogPostDetailView
 
 app_name = "blog"
 urlpatterns = [
+	path("post/<slug:slug>/", BlogPostDetailView.as_view(), name="detail"),
 	path("", AllBlogPostsView.as_view(), name="all-posts")
 ]
