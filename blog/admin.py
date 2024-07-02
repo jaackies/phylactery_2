966d4b1a905b973e90fab5c059ab1c3ab3157e90
Donald Sutherland <donald@sutherland.id.au>
2024-07-02 23:51:23 +0800

    Sets the markdown field to use monospace font

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
+from django.db import models
+from django.forms.widgets import Textarea
 from .models import MailingList, BlogPost
+
+
+class MarkdownWidget(Textarea):
+	"""
+	Custom Widget for Markdown Stuff
+	"""
+	def __init__(self):
+		super().__init__(attrs={"style": "width: 100%; font-family: monospace, monospace;"})
 
 
 class BlogPostAdmin(admin.ModelAdmin):
 	model = BlogPost
 	prepopulated_fields = {"slug_title": ("title",)}
+	formfield_overrides = {
+		models.TextField: {"widget": MarkdownWidget}
+	}
 
 
 class MailingListAdmin(admin.ModelAdmin):
