905812b159ccd4d25a021eaf520d450fc82758c3
donsuth <donald@sutherland.id.au>
2024-07-11 13:02:29 +0800

    Redirect all admin logins to the main login view

--- phylactery/urls.py
+++ phylactery/urls.py
@@ -3,6 +3,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
+# This is to ensure that all attempts to access the admin
+# go through the regular login view first.
+# Otherwise, people would need to login with a username, not an email.
+from allauth.account.decorators import secure_admin_login
+admin.autodiscover()
+admin.site.login = secure_admin_login(admin.site.login)
+
 urlpatterns = [
 	path("admin/", admin.site.urls),
 	path("accounts/", include("allauth.urls")),
