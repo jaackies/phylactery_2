60d30a58631aaa111b3f45e4499f3ba7dba305b3
donsuth <donald@sutherland.id.au>
2024-07-11 15:48:06 +0800

    Redirect to my_profile after connecting a discord account

--- accounts/adapters.py
+++ accounts/adapters.py
@@ -2,7 +2,7 @@ from allauth.account.adapter import DefaultAccountAdapter
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from allauth.socialaccount.models import SocialAccount
 from allauth.core.exceptions import ImmediateHttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, reverse
 from django.contrib import messages
 
 class CustomRegularAccountAdapter(DefaultAccountAdapter):
@@ -35,3 +35,10 @@ class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
 				# They have an account already
 				messages.error(request, "You already have a Discord account linked.")
 				raise ImmediateHttpResponse(redirect("members:my_profile"))
+	
+	def get_connect_redirect_url(self, request, socialaccount):
+		"""
+		Redirects to the User's profile after connecting an account.
+		"""
+		url = reverse("members:my_profile")
+		return url
