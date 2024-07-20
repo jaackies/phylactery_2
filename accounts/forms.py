5321b1ec6080ec462c0289a6b477ec5c9b7e3ca5
Donald Sutherland <donald@sutherland.id.au>
2024-07-20 14:38:26 +0800

    Major changes - switched to using crispy entirely for form layout

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,7 +9,8 @@ from allauth.account.models import EmailAddress
 from allauth.socialaccount.forms import DisconnectForm
 from allauth.socialaccount.models import SocialAccount
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Fieldset
+from crispy_forms.layout import Layout, Field, ButtonHolder
+from crispy_forms.bootstrap import StrictButton, FieldWithButtons
 
 
 class UnigamesUserCreationForm(UserCreationForm):
@@ -97,7 +98,7 @@ class PrototypeEmailChangeForm(UserForm):
 		
 
 class UnigamesEmailChangeForm(AddEmailForm):
-	current_email_address = forms.EmailField(
+	display_verified_email_address = forms.EmailField(
 		label="Current Email",
 		required=False,
 		disabled=True,
@@ -106,6 +107,18 @@ class UnigamesEmailChangeForm(AddEmailForm):
 				"type": "email"
 			}
 		)
+	)
+	
+	display_pending_email_address = forms.EmailField(
+		label="Current Email",
+		required=False,
+		disabled=True,
+		widget=forms.TextInput(
+			attrs={
+				"type": "email"
+			}
+		),
+		help_text="Your email address is still pending verification."
 	)
 	
 	email = forms.EmailField(
@@ -121,8 +134,40 @@ class UnigamesEmailChangeForm(AddEmailForm):
 	
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
+		
+		pending_email_address = EmailAddress.objects.get_new(self.user)  # An email attached to the user, but not verified
+		verified_email_address = EmailAddress.objects.get_verified(self.user)  # A currently verified email
+		
 		self.helper = FormHelper()
 		self.helper.form_tag = False
-		self.helper.layout = Layout(
-			"email"
-		)
+		self.helper.layout = Layout()
+		
+		if verified_email_address:
+			self.initial.update({"display_verified_email_address": verified_email_address.email})
+			self.helper.layout.append(Field("display_verified_email_address"))
+		if pending_email_address:
+			self.initial.update({"display_pending_email_address": pending_email_address.email})
+			self.helper.layout.append(Field("display_pending_email_address"))
+			button_holder = ButtonHolder()
+			button_holder.append(
+				StrictButton(
+					name="action_send",
+					content="Resend Verification",
+					input_type="submit",
+					form="pending-email",
+					css_class="btn-secondary btn-sm"
+				)
+			)
+			if verified_email_address:
+				self.fields["display_pending_email_address"].label = "Changing to"
+				button_holder.append(
+					StrictButton(
+						name="action_remove",
+						content="Cancel Change",
+						input_type="submit",
+						form="pending-email",
+						css_class="btn-danger btn-sm"
+					)
+				)
+			self.helper.layout.append(button_holder)
+		self.helper.layout.append(Field("email"))
