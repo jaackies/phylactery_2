827f94ae635bd2efc6a1e4b96f0f798f6f54eca9
Donald Sutherland <donald@sutherland.id.au>
2024-07-21 12:58:51 +0800

    More base prototyping

---
+++ control_panel/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class ControlPanelForm(forms.Form):
+	pass
+
--- control_panel/views.py
+++ control_panel/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, FormView
 
-# Create your views here.
+
+class ControlPanelListView(TemplateView):
+	"""
+	This view renders the list of control panel forms that
+	the user has access to.
+	"""
+	template_name = "control_panel/control_panel_list.html"
+
+
+class ControlPanelFormView(FormView):
+	"""
+	This view renders the requested form.
+	"""
+	template_name = "control_panel/control_panel_form.html"
