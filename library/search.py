8c1185d71045d84c8734af3faf64e5e44b19af4d
Donald Sutherland <donald@sutherland.id.au>
2024-09-11 23:33:03 +0800

    Layout basic syntax

--- library/search.py
+++ library/search.py
@@ -1,5 +1,39 @@
-from parsy import generate, regex, string, fail
+from parsy import generate, regex, string, fail, digit
 from datetime import date
+
+
+"""
+
+Search options:
+	Item is a specific type:
+		is:boardgame | is:bg
+		is:cardgame | is:cg
+		is:book | is:bk
+		is:other
+	Item has tag `dice-d20`:
+		tag:dice-d20
+		tag:"dice-d20"
+	Item does not have tag `dice-d20`:
+		-tag:tag-name
+	Item has `stuff` in name:
+		name:stuff
+		name:"stuff"
+	Item has `stuff` in full text (name + description):
+		text:stuff
+		text:"stuff"
+		
+		"stuff"
+		stuff
+			Quoted or unquoted text counts as just full text
+	Item supports 4 players:
+		players:4
+		p:4
+			This implicitly excludes all items without a player count
+	Item can be played in 15 minutes (the item's average play time is less than 15 minutes):
+		time:15
+
+"""
+
 
 year = regex(r"[0-9]{4}").map(int).desc("4 digit year")
 month = regex(r"[0-9]{2}").map(int).desc("2 digit month")
