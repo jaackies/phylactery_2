d2d90c623909313e449ec285a3dc5028ba548ecf
donsuth <donald@sutherland.id.au>
2024-09-12 17:10:03 +0800

    Commit current work on a generator function

--- library/search.py
+++ library/search.py
@@ -81,44 +81,23 @@ expression = quoted_text | keyword_expression | unquoted_text
 
 any_expression = seq(expression << or_separator, expression).combine(AnyOf)
 
+@generate
+def parse_expression():
+	any_whitespace = regex(r"\s*")
+	
+	@generate
+	def group():
+		yield string("(")
+		yield any_whitespace
+		expressions = yield parse_expression
+		yield any_whitespace
+		yield string(")")
+		return tuple(expressions)
+	
+	
+	
+
+
 if __name__ == "__main__":
 	print(keyword_expression.parse("is:book"))
-
-
-year = regex(r"[0-9]{4}").map(int).desc("4 digit year")
-month = regex(r"[0-9]{2}").map(int).desc("2 digit month")
-day = regex(r"[0-9]{2}").map(int).desc("2 digit day")
-dash = string("-")
-optional_dash = dash.optional()
-
-
-@generate
-def fulldate():
-	y = yield year
-	yield dash
-	m = yield month
-	yield dash
-	d = yield day
-	return date(y,m,d)
-
-@generate
-def full_or_partial_date():
-	d = None
-	m = None
-	y = yield year
-	dash1 = yield optional_dash
-	if dash1 is not None:
-		m = yield month
-		dash2 = yield optional_dash
-		if dash2 is not None:
-			d = yield day
-	if m is not None:
-		if m < 1 or m > 12:
-			return fail("Month must be in 1..12")
-	if d is not None:
-		try:
-			date(y, m, d)
-		except ValueError as e:
-			return fail(e.args[0])
-	return y, m, d
 
