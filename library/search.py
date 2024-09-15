7efe9bb1cf08a8318eb6771065e3308b6da52e53
Donald Sutherland <donald@sutherland.id.au>
2024-09-15 19:37:55 +0800

    Restructure to enable a self-reference

--- library/search.py
+++ library/search.py
@@ -242,6 +242,7 @@ class SearchQueryManager:
 	def __init__(self, query=""):
 		self.warnings = []
 		self.errors = []
+		self.is_done = False
 		self.query = query.lower()
 		
 	def add_warning(self, warning):
@@ -252,134 +253,131 @@ class SearchQueryManager:
 		# Adds an error to the manager.
 		self.errors.append(error)
 	
-	@generate("EXPR")
-	def parse_expression(self):
+	def evaluate(self):
 		"""
-			Parse through the search string.
-			We do the following things in order:
-				1. Consume any extra whitespace
-				2. Process the next expression we find. Check the following in order:
-					a. Check if the next character is a left bracket.
-						1. If so, use regex to capture the entire group and process it recursively.
-						2. If the regex fails to capture, there's an unmatched bracket. Raise Exception.
-					b. Quoted Text (interpreted as a text: keyword expression)
-					c. A Keyword Expression (in the form of keyword:argument)
-					d. Unquoted Text (treated the same)
-					e. End of the Line (we stop processing)
-					f. Something else: We capture until the next whitespace or word boundary, and ignore whatever we found.
-				3. If current operation is:
-					a. AND (default):
-						1. Add the results to the list of processed tokens.
-					b. OR
-						1. Wrap the list of processed tokens into "AllOf" object, and append that to the current expression.
-						2. Clear the list of processed tokens.
-						3. Add the new token to the list of processed tokens.
-						4. Reset the current operation to "AND".
-				4. Check the next character for these, in order:
-					a. If we see a right bracket, there's an unmatched bracket. Raise Exception.
-					b. End of Line: break processing.
-					c. OR seperator: Set current operation to OR.
-					d. AND seperator (which can be whitespace or a word boundary): proceed normally.
-				5. Repeat until we break processing.
-				6. Add the last tokens we've seen into the current expression, and return it.
-				7. Done!
+		This is structured weirdly for a number of reasons:
+			- We need to use the generator syntax of Parsy.
+			- We need to add errors/warnings as we find them to the Manager.
+			- In order to do the above, we need to pass a reference to the instance to the parser.
+			- ...Which we cannot do because Parsy's generator decorator is poorly structured.
+		Rather than write a whole new decorator, I decided to embed the generator-parser
+		inside a function that has the instance passed to it already.
+		
+		Apologies in advance, future maintainers of this code.
 		"""
-		current_operation = "AND"
-		processed_tokens = []
-		resulting_expression = []
-		
-		@generate("GROUP")
-		def group():
-			# We are trying to process a group. First, optionally, check if it should be inverted.
-			is_inverted = yield inverse_dash.result(True).optional(False)
-			# Then, check if the next character is an open bracket.
-			yield string("(")
-			# Since it is, we'll see if we can capture the whole group.
-			inner_expression_type, inner_expression = yield self.parse_expression.tag("EXPR")
-			# Finally, catch the closing bracket.
-			closing_bracket = yield string(")").optional()
-			if closing_bracket is None:
-				# Bracket mismatch: Raise an Exception.
-				raise UnbalancedParenthesesException("Mismatched brackets.")
+		@generate("EXPR")
+		def parse_expression():
+			"""
+				Parse through the search string.
+				We do the following things in order:
+					1. Consume any extra whitespace
+					2. Process the next expression we find. Check the following in order:
+						a. Check if the next character is a left bracket.
+							1. If so, use regex to capture the entire group and process it recursively.
+							2. If the regex fails to capture, there's an unmatched bracket. Raise Exception.
+						b. Quoted Text (interpreted as a text: keyword expression)
+						c. A Keyword Expression (in the form of keyword:argument)
+						d. Unquoted Text (treated the same)
+						e. End of the Line (we stop processing)
+						f. Something else: We capture until the next whitespace or word boundary, and ignore whatever we found.
+					3. If current operation is:
+						a. AND (default):
+							1. Add the results to the list of processed tokens.
+						b. OR
+							1. Wrap the list of processed tokens into "AllOf" object, and append that to the current expression.
+							2. Clear the list of processed tokens.
+							3. Add the new token to the list of processed tokens.
+							4. Reset the current operation to "AND".
+					4. Check the next character for these, in order:
+						a. If we see a right bracket, there's an unmatched bracket. Raise Exception.
+						b. End of Line: break processing.
+						c. OR seperator: Set current operation to OR.
+						d. AND seperator (which can be whitespace or a word boundary): proceed normally.
+					5. Repeat until we break processing.
+					6. Add the last tokens we've seen into the current expression, and return it.
+					7. Done!
+			"""
+			current_operation = "AND"
+			processed_tokens = []
+			resulting_expression = []
+			
+			@generate("GROUP")
+			def group():
+				# We are trying to process a group. First, optionally, check if it should be inverted.
+				is_inverted = yield inverse_dash.result(True).optional(False)
+				# Then, check if the next character is an open bracket.
+				yield string("(")
+				# Since it is, we'll see if we can capture the whole group.
+				inner_expression_type, inner_expression = yield parse_expression.tag("EXPR")
+				# Finally, catch the closing bracket.
+				closing_bracket = yield string(")").optional()
+				if closing_bracket is None:
+					# Bracket mismatch: Raise an Exception.
+					raise UnbalancedParenthesesException("Mismatched brackets.")
+				else:
+					# Return the results of the processed inner expression.
+					if is_inverted:
+						inner_expression.invert()
+					return inner_expression_type, inner_expression
+			
+			def add_processed_tokens_to_expression():
+				if len(processed_tokens) == 1:
+					# If there's only one processed token, append it as-is to the expression.
+					resulting_expression.append(processed_tokens[0])
+				else:
+					# Otherwise, wrap all processed tokens into an "AllOf" object.
+					resulting_expression.append(AllOf(*processed_tokens))
+				# Once that's done, empty the list of processed tokens.
+				processed_tokens.clear()
+			
+			while True:
+				yield any_whitespace
+				next_token_type, next_token = yield group | expression | eol | something_else
+				match next_token_type:
+					case "EOF":
+						# End of the current line: stop processing
+						break
+					case "???":
+						# What the hell is this?
+						self.add_warning(f"Unrecognised expression: {next_token}")
+					case "EXPR" | "GROUP":
+						# Process the token that we found.
+						if current_operation == "AND":
+							# Add the token to processed tokens.
+							processed_tokens.append(next_token)
+						elif current_operation == "OR":
+							# Wrap the currently processed tokens into the expression,
+							# then add the new one and reset the operation to AND.
+							add_processed_tokens_to_expression()
+							processed_tokens.append(next_token)
+							current_operation = "AND"
+				# Token processing done, now we look for the next seperator
+				next_seperator_type, next_seperator = yield (
+						or_separator | and_separator | eol | something_else
+				)
+				match next_seperator_type:
+					case "???":
+						self.add_warning(f"Unrecognised expression: {next_seperator}")
+					case "ERROR":
+						# There's an unmatched bracket: Add Error.
+						self.add_error("Unbalanced Parentheses")
+					case "EOF":
+						# End of the line: stop processing.
+						break
+					case "OR":
+						# Change the current operation
+						current_operation = "OR"
+			# Processing is done. Finalise and return the expression.
+			if processed_tokens:
+				add_processed_tokens_to_expression()
+			if len(resulting_expression) > 1:
+				return AnyOf(*resulting_expression)
+			elif len(resulting_expression) == 1:
+				return resulting_expression[0]
 			else:
-				# Return the results of the processed inner expression.
-				if is_inverted:
-					inner_expression.invert()
-				return inner_expression_type, inner_expression
-		
-		def add_processed_tokens_to_expression():
-			if len(processed_tokens) == 1:
-				# If there's only one processed token, append it as-is to the expression.
-				resulting_expression.append(processed_tokens[0])
-			else:
-				# Otherwise, wrap all processed tokens into an "AllOf" object.
-				resulting_expression.append(AllOf(*processed_tokens))
-			# Once that's done, empty the list of processed tokens.
-			processed_tokens.clear()
-		
-		while True:
-			yield any_whitespace
-			next_token_type, next_token = yield group | expression | eol | something_else
-			match next_token_type:
-				case "EOF":
-					# End of the current line: stop processing
-					break
-				case "???":
-					# What the hell is this?
-					self.add_warning(f"Unrecognised expression: {next_token}")
-				case "EXPR" | "GROUP":
-					# Process the token that we found.
-					if current_operation == "AND":
-						# Add the token to processed tokens.
-						processed_tokens.append(next_token)
-					elif current_operation == "OR":
-						# Wrap the currently processed tokens into the expression,
-						# then add the new one and reset the operation to AND.
-						add_processed_tokens_to_expression()
-						processed_tokens.append(next_token)
-						current_operation = "AND"
-			# Token processing done, now we look for the next seperator
-			next_seperator_type, next_seperator = yield (
-					or_separator | and_separator | eol | something_else
-			)
-			match next_seperator_type:
-				case "???":
-					self.add_warning(f"Unrecognised expression: {next_seperator}")
-				case "ERROR":
-					# There's an unmatched bracket: Add Error.
-					self.add_error("Unbalanced Parentheses")
-				case "EOF":
-					# End of the line: stop processing.
-					break
-				case "OR":
-					# Change the current operation
-					current_operation = "OR"
-		# Processing is done. Finalise and return the expression.
-		if processed_tokens:
-			add_processed_tokens_to_expression()
-		if len(resulting_expression) > 1:
-			return AnyOf(*resulting_expression)
-		elif len(resulting_expression) == 1:
-			return resulting_expression[0]
-		else:
-			return None
-	
+				return None
+		return parse_expression.parse(self.query)
 
-
-def evaluate_search_query(search_query):
-	"""
-	Expression parser is nested inside so that Warnings can be captured.
-	"""
-	warnings: list[str | UnbalancedParenthesesException] = []
-	
-	
-	try:
-		search_query_results = parse_expression.parse(search_query)
-	except UnbalancedParenthesesException as e:
-		# Do something with the error
-		search_query_results = None
-		warnings.append(e)
-	return search_query_results, warnings
 
 
 def test():
@@ -392,7 +390,8 @@ def test():
 		"name:magic name:maze"
 	]
 	for query in test_queries:
-		results, warnings = evaluate_search_query(query.lower())
+		manager = SearchQueryManager(query=query)
+		results = manager.evaluate()
 		if results is not None:
 			print(results)
 			resolved_results = results.resolve()
