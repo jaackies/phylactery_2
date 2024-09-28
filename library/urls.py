6bb318b15ffb09381295c6482c166794a4a72f9a
Donald Sutherland <donald@sutherland.id.au>
2024-09-28 15:08:42 +0800

    Remove the / from the search url

--- library/urls.py
+++ library/urls.py
@@ -25,7 +25,6 @@ from library.views import (
 app_name = 'library'
 urlpatterns = [
 	path("dashboard/", DashboardView.as_view(), name="dashboard"),
-	path("search/", ItemSearchView.as_view(), name="search"),
 	path("request/external/", ExternalReservationRequestView.as_view(), name="reservation_external"),
 	path("request/internal/", InternalReservationRequestView.as_view(), name="reservation_internal"),
 	path("approve/<int:pk>/", ReservationApprovalView.as_view(), name="approve_reservation"),
@@ -39,6 +38,7 @@ urlpatterns = [
 	path("borrow/reservation/internal/<int:pk>/", InternalReservationBorrowItemsWizard.as_view(), name="borrow_internal_reservation"),
 	path("borrow/reservation/external/<int:pk>/", ExternalReservationBorrowItemsWizard.as_view(), name="borrow_external_reservation"),
 	path("return/<int:pk>/", ReturnItemsView.as_view(), name="return"),
+	path("search", ItemSearchView.as_view(), name="search"),
 	path("autocomplete_item", ItemAutocomplete.as_view(), name="autocomplete_item"),
 	path("autocomplete_tag", LibraryTagAutocomplete.as_view(), name="autocomplete_tag"),
 	path("", LibraryHomeView.as_view(), name="home"),
