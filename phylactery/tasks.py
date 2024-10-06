179000f1aea83e60673ace899816cba0cabde42d
Donald Sutherland <donald@sutherland.id.au>
2024-10-06 12:49:05 +0800

    Add the mail sending tasks

--- phylactery/tasks.py
+++ phylactery/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 from celery.utils.log import get_task_logger
 from django.contrib.sites.models import Site
+from django.core.mail import get_connection, send_mail, send_mass_mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 import css_inline
@@ -35,4 +36,40 @@ def send_single_email_task(email_address, subject, message, html_message=None, c
 	"""
 		Sends a single email to a single email address asynchronously.
 	"""
-	logger.info(f"Sending email.\nTo: {email_address}\nSubject: {subject}\nMessage: {message}")
+	if connection is None:
+		connection = get_connection()
+		connection.open()
+
+	send_mail(
+		subject=subject,
+		message=message,
+		from_email=None,
+		recipient_list=[email_address],
+		html_message=html_message,
+		connection=connection
+	)
+	if log:
+		logger.info(f"Sent email to {email_address}.")
+
+
+@shared_task(name="send_mass_email_task")
+def send_mass_email_task(email_address_list, subject, message, html_message=None):
+	"""
+		Sends a single email to many email addresses.
+		Intended for mailing list purposes.
+		It calls the send_single_email_task a bunch of times,
+		so that everything goes through its rate limit.
+	"""
+	connection = get_connection()
+	connection.open()
+	for email_address in email_address_list:
+		send_single_email_task.delay(
+			email_address=email_address,
+			subject=subject,
+			message=message,
+			html_message=html_message,
+			connection=connection,
+			log=False
+		)
+	connection.close()
+	logger.info(f"Sent emails to {len(email_address_list)} recipients.")
