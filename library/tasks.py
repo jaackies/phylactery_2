7f4e8526f3269610906c296d88e5598960a1f7d7
donsuth <donald@sutherland.id.au>
2024-10-08 21:25:59 +0800

    Update return receipt - not hooked up yet

--- library/tasks.py
+++ library/tasks.py
@@ -21,3 +21,23 @@ def send_borrow_receipt(email_address, borrower_name, items, authorised_by):
 		message=plaintext_message,
 		html_message=html_message
 	)
+
+
+def send_return_receipt(email_address, borrower_name, items, authorised_by):
+	context = {
+		"borrower_name": borrower_name,
+		"items": items,
+		"gatekeeper": authorised_by,
+		"today": timezone.now(),
+	}
+	subject = "Unigames Library Return Receipt"
+	plaintext_message, html_message = render_html_email(
+		template_name="library/email/return_receipt.html",
+		context=context,
+	)
+	send_single_email_task.delay_on_commit(
+		email_address=email_address,
+		subject=subject,
+		message=plaintext_message,
+		html_message=html_message
+	)
