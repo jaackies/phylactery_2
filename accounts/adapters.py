5547989a37bdcb3798b909e9aa4406e49a532181
donsuth <donald@sutherland.id.au>
2024-10-08 19:32:51 +0800

    Account already exists email

--- accounts/adapters.py
+++ accounts/adapters.py
@@ -1,4 +1,5 @@
 from allauth.account.adapter import DefaultAccountAdapter
+from allauth.account.internal import flows
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from allauth.socialaccount.models import SocialAccount
 from allauth.core.exceptions import ImmediateHttpResponse
@@ -53,12 +54,31 @@ class CustomRegularAccountAdapter(DefaultAccountAdapter):
 			html_message=html_message,
 		)
 		
-	
 	def send_notification_mail(self, template_prefix, user, context=None, email=None):
-		pass
+		# Disabled in our app
+		return
 	
 	def send_account_already_exists_mail(self, email):
-		pass
+		password_reset_url = flows.password_reset.get_reset_password_url(
+			self.request,
+		)
+		context = {
+			"password_reset_url": password_reset_url,
+			"email": email,
+		}
+		subject = "Unigames Account Already Exists"
+		plaintext_message, html_message = render_html_email(
+			template_name="account/email/account_already_exists.html",
+			context=context,
+			request=self.request,
+		)
+		send_single_email_task.delay(
+			email_address=email,
+			subject=subject,
+			message=plaintext_message,
+			html_message=html_message,
+		)
+		
 	
 	def send_mail(self, template_prefix, email, context):
 		"""
