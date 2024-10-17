de1a3474dfcb1bdc708fda6d995d433708932328
donsuth <donald@sutherland.id.au>
2024-10-17 13:03:15 +0800

    Add a reason to blog post emails

--- blog/tasks.py
+++ blog/tasks.py
@@ -22,9 +22,28 @@ def send_pending_email_orders_task():
 		if order.is_ready:
 			subject = f"{order.blog_post.title} - Unigames News"
 			members_to_email_to = order.get_members_to_send_to()
+			
+			# Make a nice string to tell the members why they are receiving the email.
+			if order.mailing_lists.count() == 0:
+				reason = "This message was sent to all Unigames members that have 'Optional Emails' turned on."
+			elif order.mailing_lists.count() == 1:
+				list_name = order.mailing_lists.first().name
+				reason = (
+					f"This message was sent to you because you are subscribed to the '{list_name}' mailing list, "
+					f"and have 'Optional Emails' turned on."
+				)
+			else:
+				list_names = ", ".join(order.mailing_lists.values_list("name", flat=True))
+				reason = (
+					f"This message was sent to you because you have 'Optional Emails' turned on, "
+					f"and are subscribed to one or more of these mailing lists: {list_names}"
+				)
+			
 			context = {
 				"blogpost": order.post,
+				"reason": reason,
 			}
+			
 			plaintext_message, html_message = render_html_email(
 				template_name="blog/email/blog_post.html",
 				context=context,
