15854e642ee1b0ecc4c698c2e09608904ee6f1b0
donsuth <donald@sutherland.id.au>
2024-11-19 13:29:20 +0800

    Fix celery not finding tasks

--- phylactery/tasks.py
+++ phylactery/tasks.py
@@ -1,3 +1,3 @@
-
-
-
+# These tasks are imported here so that celery can auto-discover them.
+from phylactery.communication.discord import send_to_discord
+from phylactery.communication.email import send_single_email_task, send_mass_email_task
