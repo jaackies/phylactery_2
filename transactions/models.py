400704b681de82b3f07ea2dbd5cbab232c5c2396
jaackies <83699536+jaackies@users.noreply.github.com>
2024-12-03 15:55:51 +0800

    Add transactions app

---
+++ transactions/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from members.models import Member
+
+# Create your models here.
+
+class Transaction(models.Model):
+  '''comment here'''
+  member = models.ForeignKey(Member, on_delete=models.SET_NULL, related_name="transactions", null=True, blank=True)
+
+  description = models.CharField(
+    max_length = 200)
+
+  reference_number = models.CharField(
+    max_length = 20)
+
+  amount = models.DecimalField(
+    max_digits=6,
+    decimal_places=2)
+
+  transaction_date = models.DateTimeField(
+    auto_now_add=True)
