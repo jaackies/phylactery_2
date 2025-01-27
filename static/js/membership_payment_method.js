92f16d8a75ceee583619f3052612dc59c04a53f4
donsuth <donald@sutherland.id.au>
2025-01-27 19:31:33 +1300

    Add the hiding and showing functions, and add events to trigger them

--- static/js/membership_payment_method.js
+++ static/js/membership_payment_method.js
@@ -1,10 +1,11 @@
 // Script for the membership form
 // Shows / Hides the relevant fields based on which option is selected.
 
-const checkPaymentMethod = () => {
-	const cash_button = document.querySelector("input[name='preview-cash_or_transfer'][value='cash']");
-	const transfer_button = document.querySelector("input[name='preview-cash_or_transfer'][value='transfer']");
+const cash_button = document.querySelector("input[name='preview-cash_or_transfer'][value='cash']");
+const transfer_button = document.querySelector("input[name='preview-cash_or_transfer'][value='transfer']");
 
+
+const checkPaymentMethod = () => {
 	const transfer_panel = document.querySelector("#reference-code-card");
 	const checklist_panel = document.querySelector("#checklist-card");
 	const payment_text = document.querySelector("#payment_method_text");
@@ -27,5 +28,22 @@ const checkPaymentMethod = () => {
 		payment_text.textContent = "";
 	}
 
+	if (show_transfer_panel) {
+		transfer_panel.classList.remove("d-none");
+	}
+	else {
+		transfer_panel.classList.add("d-none");
+	}
+
+	if (show_checklist_panel) {
+		checklist_panel.classList.remove("d-none");
+	}
+	else {
+		checklist_panel.classList.add("d-none");
+	}
 
 }
+
+document.addEventListener("DOMContentLoaded", checkPaymentMethod);
+cash_button.addEventListener("change", checkPaymentMethod);
+transfer_button.addEventListener("change", checkPaymentMethod);
